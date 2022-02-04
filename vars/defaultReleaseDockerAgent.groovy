114c2b39fd47d229e896e893eee4d7d38840480a
Peter Nied <petern@amazon.com>
2022-02-04 17:31:42 +0000

    DockerAgent
    Signed-off-by: Peter Nied <petern@amazon.com>

---
+++ vars/defaultReleaseDockerAgent.groovy
@@ -0,0 +1,7 @@
+void call(Map args = [:]) {
+    return docker {
+            label 'Jenkins-Agent-al2-x64-c54xlarge-Docker-Host'
+            image 'opensearchstaging/ci-runner:ci-runner-centos7-v1'
+            alwaysPull true
+        }
+}
