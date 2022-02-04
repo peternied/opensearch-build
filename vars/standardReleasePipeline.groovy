e0fddc974f72f937888bb72df77b14129e1fc564
Peter Nied <petern@amazon.com>
2022-02-04 21:20:01 +0000

    Much cleaner pipeline standard with potential features
    Signed-off-by: Peter Nied <petern@amazon.com>

---
+++ vars/standardReleasePipeline.groovy
@@ -0,0 +1,39 @@
+/** A standard release pipeline for OpenSearch projects */
+
+void call(Map args = [:], Closure body) {
+    pipeline {
+        agent any
+        // Note; testing this on custom jenkins instance that doesn't
+        // have docker installed
+        // {
+        //     docker {
+        //         label 'AL2-X64'
+        //         image args.overrideDockerImage ?: 'alpine:latest'
+        //         alwaysPull true
+        //     }
+        // }
+        options {
+            timeout(time: 1, unit: 'HOURS')
+        }
+        stages{
+            stage("Release ${JOB-NAME}") {
+                steps {
+                    script {
+                        body()
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                script {
+                    postCleanup()
+                    // Update to also do docker cleanup too
+                    if (args.additionalCleanup) {
+                        additionalCleanup()
+                    }
+                }
+            }
+        }
+    }
+}
