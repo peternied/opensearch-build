46e851ce4550e58c9fd9aafdf5b0b4baddd72d96
Peter Nied <petern@amazon.com>
2022-02-04 20:17:26 +0000

    Default Pipeline no docker
    Signed-off-by: Peter Nied <petern@amazon.com>

--- vars/defaultPipeline.groovy
+++ vars/defaultPipeline.groovy
@@ -1,21 +1,19 @@
 void call(def stageName, Closure body) {
     return  pipeline {
-        agent {
-            docker {
-                label 'Jenkins-Agent-al2-x64-c54xlarge-Docker-Host'
-                image 'opensearchstaging/ci-runner:centos7-x64-arm64-jdkmulti-node10.24.1-cypress6.9.1-20211028'
-                alwaysPull true
-            }
-        }
-        stages(stageName) {
-            stage {
-                body()
+        agent any
+        stages{
+            stage(stageName) {
+                steps {
+                    script {
+                        body()
+                    }
+                }
             }
         }
         post {
             always {
                 script {
-                    postCleanUp() // Update to also do docker cleanup too
+                    postCleanup() // Update to also do docker cleanup too
                 }
             }
         }
